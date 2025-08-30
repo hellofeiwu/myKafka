1583872064db0a67aa5773c538b2b869e9be93a0
hellofeiwu <hellofeiwu@gmail.com>
2025-08-30 17:09:08 +0800

    实现收发消息，有两个秘钥相关文件在本地，没有提交上来

---
+++ quickStart/src/main/java/com/imooc/myConsumer.java
@@ -0,0 +1,56 @@
+package com.imooc;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.*;
+
+public class myConsumer {
+    public static void main(String[] args) {
+        String TOPIC_NAME = "default_topic";
+        String TRUSTSTORE_PASSWORD = "123456";
+
+        String sasl_username = "avnadmin";
+        String sasl_password = "AVNS_qSH1VzUsyh5Ox5CqvDT";
+        String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+        String jaasConfig = String.format(jaasTemplate, sasl_username, sasl_password);
+
+        Properties properties = new Properties();
+
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("sasl.jaas.config", jaasConfig);
+        properties.setProperty("ssl.endpoint.identification.algorithm", "");
+        properties.setProperty("ssl.truststore.type", "jks");
+        properties.setProperty("ssl.truststore.location", "client.truststore.jks");
+        properties.setProperty("ssl.truststore.password", TRUSTSTORE_PASSWORD);
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-a944bd8-figo357159-63d6.f.aivencloud.com:20293");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"my-group");
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        int messageCount = 0;
+        while (messageCount<10){
+            ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(1000));
+            for(TopicPartition p : records.partitions()){
+                List<ConsumerRecord<String, String>> recordList = records.records(p);
+                for(ConsumerRecord<String, String> r : recordList){
+                    messageCount++;
+                    System.out.println(r.value());
+                }
+            }
+        }
+        consumer.close();
+    }
+}
---
+++ quickStart/src/main/java/com/imooc/myProducer.java
@@ -0,0 +1,51 @@
+package com.imooc;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class myProducer {
+    public static void main(String[] args) {
+        String TOPIC_NAME = "default_topic";
+        String TRUSTSTORE_PASSWORD = "123456";
+
+        String sasl_username = "avnadmin";
+        String sasl_password = "AVNS_qSH1VzUsyh5Ox5CqvDT";
+        String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+        String jaasConfig = String.format(jaasTemplate, sasl_username, sasl_password);
+
+        Properties properties = new Properties();
+
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("sasl.jaas.config", jaasConfig);
+        properties.setProperty("ssl.endpoint.identification.algorithm", "");
+        properties.setProperty("ssl.truststore.type", "jks");
+        properties.setProperty("ssl.truststore.location", "client.truststore.jks");
+        properties.setProperty("ssl.truststore.password", TRUSTSTORE_PASSWORD);
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-a944bd8-figo357159-63d6.f.aivencloud.com:20293");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i=0; i<3; i++){
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<>(TOPIC_NAME, "**** hello from idea "+i+" ****");
+
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if(e != null){
+                        e.printStackTrace();
+                    }
+                    System.out.println("===== sent ok! =====");
+                }
+            });
+        }
+        producer.flush();
+        producer.close();
+    }
+}
