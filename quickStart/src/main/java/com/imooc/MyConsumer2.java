3d30fd2ea0b47b143bc136add124a5b8e92d922a
hellofeiwu <hellofeiwu@gmail.com>
2025-09-01 08:47:12 +0800

    使用不同 消费者组 实验

---
+++ quickStart/src/main/java/com/imooc/MyConsumer2.java
@@ -0,0 +1,58 @@
+package com.imooc;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class MyConsumer2 {
+    public static void main(String[] args) {
+        String TOPIC_NAME = "default_topic";
+        String TRUSTSTORE_PASSWORD = "123456";
+
+        String sasl_username = "avnadmin";
+        String sasl_password = "AVNS_qSH1VzUsyh5Ox5CqvDT";
+        String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+        String jaasConfig = String.format(jaasTemplate, sasl_username, sasl_password);
+
+        Properties properties = new Properties();
+
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.mechanism", "SCRAM-SHA-256");
+        properties.setProperty("sasl.jaas.config", jaasConfig);
+        properties.setProperty("ssl.endpoint.identification.algorithm", "");
+        properties.setProperty("ssl.truststore.type", "jks");
+        properties.setProperty("ssl.truststore.location", "client.truststore.jks");
+        properties.setProperty("ssl.truststore.password", TRUSTSTORE_PASSWORD);
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-a944bd8-figo357159-63d6.f.aivencloud.com:20293");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,MyDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,"my-group-2");
+        properties.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG,MyConsumerInterceptor.class.getName());
+
+        KafkaConsumer<String,User> consumer = new KafkaConsumer<String, User>(properties);
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        int loopCount = 0;
+        while (loopCount<20){
+            System.out.println("enter while loop: "+loopCount);
+            ConsumerRecords<String,User> records = consumer.poll(Duration.ofMillis(2000));
+            for(TopicPartition p : records.partitions()){
+                List<ConsumerRecord<String, User>> recordList = records.records(p);
+                for(ConsumerRecord<String, User> r : recordList){
+                    System.out.println("topic: "+r.topic() + ", partition id: "+r.partition() +", message: "+r.value().name);
+                }
+            }
+            loopCount++;
+        }
+        consumer.close();
+    }
+}
--- quickStart/src/main/java/com/imooc/MyProducer.java
+++ quickStart/src/main/java/com/imooc/MyProducer.java
@@ -31,7 +31,7 @@ public class MyProducer {
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,MySerializer.class.getName());
         properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, MyProducerInterceptor.class.getName());
-        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,MyPartitioner.class.getName());
+        //properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG,MyPartitioner.class.getName());
 
         KafkaProducer<String, User> producer = new KafkaProducer<String, User>(properties);
 
